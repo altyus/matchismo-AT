9e09dbd5b2ea3af85ec4f27df0683b8952d91052
altyus <alctyus@gmail.com>
2013-01-31 23:47:13 -0500

    Initial Commit

---
+++ StanfordCardGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StanfordCardGame
+//
+//  Created by Al Tyus on 1/31/13.
+//  Copyright (c) 2013 Al Tyus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
